52556c160c15921c8db180e6067b8910454fedf6
Gonzalo Monar <gonza.monar@gmail.com>
2025-06-29 13:30:44 -0300

    [notification-service]chore: upload Spring project base

---
+++ services/notification-service/src/test/java/com/clinica_hipocrates/notification_service/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clinica_hipocrates.notification_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
