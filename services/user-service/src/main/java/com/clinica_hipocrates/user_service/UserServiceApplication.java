b9c32bfb930a391f38f7005ba4af60f2dc9b5fef
Gonzalo Monar <gonza.monar@gmail.com>
2025-07-01 02:29:18 -0300

    [user-service]chore: add scanning for common.exception

--- services/user-service/src/main/java/com/clinica_hipocrates/user_service/UserServiceApplication.java
+++ services/user-service/src/main/java/com/clinica_hipocrates/user_service/UserServiceApplication.java
@@ -3,7 +3,10 @@ package com.clinica_hipocrates.user_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {
+		"com.clinica_hipocrates.user_service",
+		"com.clinica_hipocrates.common.exception"
+})
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
