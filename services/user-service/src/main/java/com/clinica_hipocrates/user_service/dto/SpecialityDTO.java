97a19bb6732e5f644d7bf57bdf34a3b13c249432
Gonzalo Monar <gonza.monar@gmail.com>
2025-07-01 02:30:39 -0300

    [user-service]feat: add Speciality DTO class

---
+++ services/user-service/src/main/java/com/clinica_hipocrates/user_service/dto/SpecialityDTO.java
@@ -0,0 +1,45 @@
+package com.clinica_hipocrates.user_service.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.hateoas.Links;
+import org.springframework.hateoas.RepresentationModel;
+
+public class SpecialityDTO extends RepresentationModel<SpecialityDTO> {
+    @Schema(description = "Unique ID of the speciality", example = "2")
+    private Long id;
+
+    @Schema(description = "Name of the speciality", example = "Cardiology")
+    @NotBlank @NotNull
+    private String name;
+
+    @Override
+    @Schema(hidden = true)
+    public Links getLinks() {
+        return super.getLinks();
+    }
+
+    public SpecialityDTO() {}
+
+    public SpecialityDTO(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
