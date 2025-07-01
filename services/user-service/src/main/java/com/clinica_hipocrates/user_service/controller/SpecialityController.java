51df9f9c06cb55c8eeff166c195cac1ea718452a
Gonzalo Monar <gonza.monar@gmail.com>
2025-07-01 02:31:26 -0300

    [user-service]feat: add Speciality Controller

---
+++ services/user-service/src/main/java/com/clinica_hipocrates/user_service/controller/SpecialityController.java
@@ -0,0 +1,94 @@
+package com.clinica_hipocrates.user_service.controller;
+
+import com.clinica_hipocrates.user_service.assembler.SpecialityDTOAssembler;
+import com.clinica_hipocrates.user_service.dto.SpecialityDTO;
+import com.clinica_hipocrates.user_service.model.Specialist;
+import com.clinica_hipocrates.user_service.model.Speciality;
+import com.clinica_hipocrates.user_service.service.SpecialityService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/specialities")
+@Tag(name = "/specialities", description = "Manage medical specialities.")
+public class SpecialityController {
+
+    @Autowired
+    private SpecialityService service;
+
+    @Autowired
+    private SpecialityDTOAssembler assembler;
+
+
+    @GetMapping
+    @Operation(summary = "Get all specialities")
+    @ApiResponse(responseCode = "200", description = "OK: List of all specialities")
+    public ResponseEntity<List<SpecialityDTO>> getAll() {
+        return ResponseEntity.ok(assembler.toListModel(service.findAll()));
+    }
+
+
+    @Operation(summary = "Get a speciality by ID")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "OK: Speciality found."),
+        @ApiResponse(responseCode = "404", description = "NOT_FOUND: Speciality doesn't exist.")
+    })
+    @GetMapping("/{id}")
+    public ResponseEntity<SpecialityDTO> getById(@PathVariable Long id) {
+        Speciality speciality = service.findById(id);
+        return (speciality != null)
+                ? ResponseEntity.ok(assembler.toModel(speciality))
+                : ResponseEntity.notFound().build();
+    }
+
+
+    @Operation(summary = "Get list of doctors by speciality ID")
+    @ApiResponse(responseCode = "200", description = "OK: List of all speciality's doctors")
+    @GetMapping("/{id}/doctors")
+    public ResponseEntity<List<Specialist>> getDoctorsBySpeciality(@PathVariable Long id) {
+        return ResponseEntity.ok(service.findDoctorsBySpeciality(id));
+    }
+
+
+    @Operation(summary = "Create a new speciality")
+    @ApiResponse(responseCode = "201", description = "CREATED: Speciality created.")
+    @ApiResponse(responseCode = "400", description = "VALIDATION_ERROR: Blank name.")
+    @ApiResponse(responseCode = "400", description = "DUPLICATE_RESOURCE: Speciality exists.")
+    @PostMapping
+    public ResponseEntity<SpecialityDTO> create(@Valid @RequestBody SpecialityDTO dtoSpeciality) {
+        Speciality savedSpeciality = service.create(assembler.toEntity(dtoSpeciality));
+        return ResponseEntity.status(HttpStatus.CREATED).body(assembler.toModel(savedSpeciality));
+    }
+
+
+    @Operation(summary = "Update a speciality by ID")
+    @ApiResponse(responseCode = "200", description = "OK: Speciality updated.")
+    @ApiResponse(responseCode = "400", description = "VALIDATION_ERROR: Blank name.")
+    @ApiResponse(responseCode = "400", description = "DUPLICATE_RESOURCE: Speciality exists.")
+    @ApiResponse(responseCode = "404", description = "NOT_FOUND: Speciality doesn't exist.")
+    @PutMapping("/{id}")
+    public ResponseEntity<SpecialityDTO> update(@PathVariable Long id, @Valid @RequestBody SpecialityDTO dtoSpeciality) {
+        Speciality updatedSpeciality = service.update(id, assembler.toEntity(dtoSpeciality));
+        return ResponseEntity.ok(assembler.toModel(updatedSpeciality));
+    }
+
+
+    @Operation(summary = "Delete a speciality by ID")
+    @ApiResponse(responseCode = "204", description = "NO_CONTENT: Speciality deleted.")
+    @ApiResponse(responseCode = "404", description = "NOT_FOUND: Speciality doesn't exist.")
+    @ApiResponse(responseCode = "409", description = "CONFLICT: SQL constraint violation.")
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+}
