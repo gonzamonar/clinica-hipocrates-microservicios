6377463a06102ccf41556bcc4dbdbe6596856e08
Gonzalo Monar <gonza.monar@gmail.com>
2025-07-05 13:24:58 -0300

    [user-service]feat: add UserService interface

---
+++ services/user-service/src/main/java/com/clinica_hipocrates/user_service/service/UserService.java
@@ -0,0 +1,13 @@
+package com.clinica_hipocrates.user_service.service;
+
+import com.clinica_hipocrates.user_service.model.User;
+
+import java.util.List;
+
+public interface UserService {
+    List<User> findAll();
+    User findById(Long id);
+    User create(User user);
+    User update(Long id, User user);
+    void delete(Long id);
+}
