7d31416ce45c4a92c3bbe629253238a2471405f9
Gonzalo Monar <gonza.monar@gmail.com>
2025-07-05 13:25:10 -0300

    [user-service]feat: add UserService implementation

---
+++ services/user-service/src/main/java/com/clinica_hipocrates/user_service/service/impl/UserServiceImpl.java
@@ -0,0 +1,75 @@
+package com.clinica_hipocrates.user_service.service.impl;
+
+import com.clinica_hipocrates.common.exception.DuplicateResourceException;
+import com.clinica_hipocrates.common.exception.ResourceNotFoundException;
+import com.clinica_hipocrates.user_service.model.User;
+import com.clinica_hipocrates.user_service.repository.UserRepository;
+import com.clinica_hipocrates.user_service.service.UserService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository repository;
+
+    public UserServiceImpl(UserRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+
+    @Override
+    public User findById(Long id) throws ResourceNotFoundException {
+        return repository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("El usuario con id " + id + " no existe."));
+    }
+
+    @Override
+    public User create(User user) throws DuplicateResourceException {
+        if (repository.existsByDni(user.getDni())) {
+            throw new DuplicateResourceException("Ya existe un usuario con ese DNI.");
+        }
+        if (repository.existsByEmail(user.getEmail())) {
+            throw new DuplicateResourceException("Ya existe un usuario con ese email.");
+        }
+        return repository.save(user);
+    }
+
+    @Override
+    public User update(Long id, User user) throws DuplicateResourceException, ResourceNotFoundException {
+        if (repository.existsByDni(user.getDni())) {
+            throw new DuplicateResourceException("Ya existe un usuario con ese DNI.");
+        }
+        if (repository.existsByEmail(user.getEmail())) {
+            throw new DuplicateResourceException("Ya existe un usuario con ese email.");
+        }
+        User updatedUser = repository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("El usuario con id " + id + " no existe."));
+        setUserFields(updatedUser, user);
+        repository.save(updatedUser);
+        return updatedUser;
+    }
+
+    private static void setUserFields(User updatedUser, User user) {
+        updatedUser.setName(user.getName());
+        updatedUser.setLastname(user.getLastname());
+        updatedUser.setAge(user.getAge());
+        updatedUser.setDni(user.getDni());
+        updatedUser.setEmail(user.getEmail());
+        updatedUser.setProfilePic(user.getProfilePic());
+        updatedUser.setSpecificFields(user);
+    }
+
+    @Override
+    public void delete(Long id) throws ResourceNotFoundException {
+        if (!repository.existsById(id)) {
+            throw new ResourceNotFoundException("El usuario con id " + id + " no existe.");
+        }
+        repository.deleteById(id);
+    }
+}
