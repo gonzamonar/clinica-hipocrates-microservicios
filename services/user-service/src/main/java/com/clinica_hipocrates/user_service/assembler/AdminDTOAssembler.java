2589726e3a5de3f6701dc1d36953f22860226dcb
Gonzalo Monar <gonza.monar@gmail.com>
2025-07-05 13:18:57 -0300

    [user-service]feat: add AdminDTOAssembler

---
+++ services/user-service/src/main/java/com/clinica_hipocrates/user_service/assembler/AdminDTOAssembler.java
@@ -0,0 +1,21 @@
+package com.clinica_hipocrates.user_service.assembler;
+
+import com.clinica_hipocrates.user_service.dto.AdminDTO;
+import com.clinica_hipocrates.user_service.model.Admin;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AdminDTOAssembler extends AbstractUserDTOAssembler<Admin, AdminDTO> {
+
+    @Override
+    protected AdminDTO createDtoInstance() { return new AdminDTO(); }
+
+    @Override
+    protected Admin createEntityInstance() { return new Admin(); }
+
+    @Override
+    protected void mapSpecificFieldsToDTO(Admin user, AdminDTO dto) { }
+
+    @Override
+    protected void mapSpecificFieldsToEntity(AdminDTO dto, Admin user) { }
+}
