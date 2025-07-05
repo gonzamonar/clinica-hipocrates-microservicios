afe7d955cdbe86c3a3bf5e86eab14a340c3b6a91
Gonzalo Monar <gonza.monar@gmail.com>
2025-07-05 13:23:44 -0300

    [user-service]feat: add custom methods to the UserRepository

--- services/user-service/src/main/java/com/clinica_hipocrates/user_service/repository/UserRepository.java
+++ services/user-service/src/main/java/com/clinica_hipocrates/user_service/repository/UserRepository.java
@@ -6,4 +6,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    boolean existsByDni(Integer dni);
+    boolean existsByEmail(String email);
 }
