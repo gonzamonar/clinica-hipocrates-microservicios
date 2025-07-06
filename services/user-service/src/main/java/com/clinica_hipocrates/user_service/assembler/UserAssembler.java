9398022aa6920de02b753cebf3a277c0479f177e
Gonzalo Monar <gonza.monar@gmail.com>
2025-07-06 16:15:26 -0300

    [user-service]style: remove unused import in UserAssembler

--- services/user-service/src/main/java/com/clinica_hipocrates/user_service/assembler/UserAssembler.java
+++ services/user-service/src/main/java/com/clinica_hipocrates/user_service/assembler/UserAssembler.java
@@ -3,8 +3,6 @@ package com.clinica_hipocrates.user_service.assembler;
 import com.clinica_hipocrates.user_service.dto.AbstractUserDTO;
 import com.clinica_hipocrates.user_service.model.User;
 
-import java.util.List;
-
 public interface UserAssembler<U extends User, D extends AbstractUserDTO> {
     D toModel(U entity);
     U toEntity(D dto);
