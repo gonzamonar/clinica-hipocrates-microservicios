800de2a5ffadb4ada7dffe343deb0d86bcbb81d8
Gonzalo Monar <gonza.monar@gmail.com>
2025-07-13 22:22:31 -0300

    [appointment-service]feat: add UserCreatedEvent listener

---
+++ services/appointment-service/src/main/java/com/clinica_hipocrates/appointment_service/messaging/listener/UserCreatedListener.java
@@ -0,0 +1,32 @@
+package com.clinica_hipocrates.appointment_service.messaging.listener;
+
+import com.clinica_hipocrates.appointment_service.event.UserCreatedEvent;
+import com.clinica_hipocrates.appointment_service.messaging.RabbitMQConfig;
+import com.clinica_hipocrates.appointment_service.model.CachedUser;
+import com.clinica_hipocrates.appointment_service.repository.CachedUserRepository;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+@Component
+public class UserCreatedListener {
+
+    private final CachedUserRepository cachedUserRepository;
+
+    public UserCreatedListener(CachedUserRepository cachedUserRepository) {
+        this.cachedUserRepository = cachedUserRepository;
+    }
+
+    @RabbitListener(queues = RabbitMQConfig.USER_CREATED_QUEUE)
+    public void handleUserCreated(UserCreatedEvent event) {
+        CachedUser user = new CachedUser();
+        user.setId(event.getId());
+        user.setUserType(event.getUserType());
+        user.setName(event.getName());
+        user.setLastname(event.getLastname());
+        user.setSpecialityIds(event.getSpecialityIds());
+        user.setLastUpdated(LocalDateTime.now());
+        cachedUserRepository.save(user);
+    }
+}
