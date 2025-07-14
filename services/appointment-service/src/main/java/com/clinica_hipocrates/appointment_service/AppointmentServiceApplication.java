f903d960f4ef947d54586720af678e641117d03d
Gonzalo Monar <gonza.monar@gmail.com>
2025-07-13 22:20:25 -0300

    [appointment-service]chore: configure scanPackages in Common.Exception

--- services/appointment-service/src/main/java/com/clinica_hipocrates/appointment_service/AppointmentServiceApplication.java
+++ services/appointment-service/src/main/java/com/clinica_hipocrates/appointment_service/AppointmentServiceApplication.java
@@ -3,7 +3,10 @@ package com.clinica_hipocrates.appointment_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {
+		"com.clinica_hipocrates.appointment_service",
+		"com.clinica_hipocrates.common.exception"
+})
 public class AppointmentServiceApplication {
 
 	public static void main(String[] args) {
