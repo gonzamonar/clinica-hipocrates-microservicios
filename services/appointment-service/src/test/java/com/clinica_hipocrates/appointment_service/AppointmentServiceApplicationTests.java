f316e354852f0373e9a75034a48e05dc4ca38b70
Gonzalo Monar <gonza.monar@gmail.com>
2025-07-13 22:26:09 -0300

    [appointment-service]test: add Application base tests

--- services/appointment-service/src/test/java/com/clinica_hipocrates/appointment_service/AppointmentServiceApplicationTests.java
+++ services/appointment-service/src/test/java/com/clinica_hipocrates/appointment_service/AppointmentServiceApplicationTests.java
@@ -3,11 +3,20 @@ package com.clinica_hipocrates.appointment_service;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
 @SpringBootTest
 class AppointmentServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
+		// Smoke test.
+		// The test will fail if the Spring application context cannot start.
+	}
+
+	@Test
+	void main_shouldStartWithoutErrors() {
+		assertDoesNotThrow(() -> AppointmentServiceApplication.main(new String[] {}));
 	}
 
 }
