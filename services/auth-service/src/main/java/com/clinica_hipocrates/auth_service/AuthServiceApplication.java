6fe1238e0d466e3a14b3da9deb0ef96fa6b49414
Gonzalo Monar <gonza.monar@gmail.com>
2025-07-16 19:45:13 -0300

    [auth-service]feat: add common.exception scanning

--- services/auth-service/src/main/java/com/clinica_hipocrates/auth_service/AuthServiceApplication.java
+++ services/auth-service/src/main/java/com/clinica_hipocrates/auth_service/AuthServiceApplication.java
@@ -3,11 +3,13 @@ package com.clinica_hipocrates.auth_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {
+		"com.clinica_hipocrates.auth_service",
+		"com.clinica_hipocrates.common.exception"
+})
 public class AuthServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AuthServiceApplication.class, args);
 	}
-
 }
