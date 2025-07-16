403016c3297a189d8638ce7e9c117dd1bfb5beaf
Gonzalo Monar <gonza.monar@gmail.com>
2025-07-16 19:50:37 -0300

    [auth-service]feat: add jwt Service

---
+++ services/auth-service/src/main/java/com/clinica_hipocrates/auth_service/security/JwtService.java
@@ -0,0 +1,71 @@
+package com.clinica_hipocrates.auth_service.security;
+
+import com.clinica_hipocrates.auth_service.model.AuthUser;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+import java.util.UUID;
+
+@Component
+public class JwtService {
+
+    @Value("${jwt.secret}")
+    private String secret;
+
+    private SecretKey getKey() {
+        return Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
+    }
+
+    public String generateToken(AuthUser user) {
+        Instant now = Instant.now();
+        return Jwts.builder()
+                .setSubject(user.getId().toString())
+                .claim("email", user.getEmail())
+                .claim("alias", user.getAlias())
+                .claim("enabled", user.getEnabled())
+                .claim("verified", user.getVerified())
+                .claim("role", user.getType().name())
+                .setIssuedAt(Date.from(now))
+                .setExpiration(Date.from(now.plus(1, ChronoUnit.HOURS)))
+                .signWith(getKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        final String email = extractUsername(token);
+        return (email.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+
+    public boolean isTokenExpired(String token) {
+        return extractAllClaims(token).getExpiration().before(new Date());
+    }
+
+    public String extractUsername(String token) {
+        return extractAllClaims(token).get("email", String.class);
+    }
+
+    public UUID extractUserId(String token) {
+        String subject = extractAllClaims(token).getSubject();
+        return UUID.fromString(subject);
+    }
+
+
+    private Claims extractAllClaims(String token) throws JwtException {
+        return Jwts.parserBuilder()
+                .setSigningKey(getKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+}
