452df1d00b2a6675bb1b3ca6bd2688f00ee29ebd
Gonzalo Monar <gonza.monar@gmail.com>
2025-07-16 19:44:04 -0300

    [gateway]feat: add basic security config / disable default

---
+++ gateway/src/main/java/com/clinica_hipocrates/gateway/security/SecurityConfig.java
@@ -0,0 +1,22 @@
+package com.clinica_hipocrates.gateway.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        return http
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(auth -> auth
+                        .anyExchange().permitAll() // JWT global filter handles auth
+                )
+                .build();
+    }
+}
