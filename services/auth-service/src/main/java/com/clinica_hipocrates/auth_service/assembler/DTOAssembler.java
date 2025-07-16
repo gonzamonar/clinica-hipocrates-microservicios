e17c6f50283e584c79b708b3fb5cf4efbfbc3c02
Gonzalo Monar <gonza.monar@gmail.com>
2025-07-16 19:47:58 -0300

    [auth-service]feat: add DTOAssembler interface

---
+++ services/auth-service/src/main/java/com/clinica_hipocrates/auth_service/assembler/DTOAssembler.java
@@ -0,0 +1,20 @@
+package com.clinica_hipocrates.auth_service.assembler;
+
+import java.util.List;
+
+public interface DTOAssembler<E, D> {
+    D toModel(E entity);
+    E toEntity(D dto);
+
+    default List<D> toModelList(List<E> entities) {
+        return entities.stream()
+                .map(this::toModel)
+                .toList();
+    }
+
+    default List<E> toEntityList(List<D> dtos) {
+        return dtos.stream()
+                .map(this::toEntity)
+                .toList();
+    }
+}
