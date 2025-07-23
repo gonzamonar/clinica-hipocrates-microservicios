f23aec1cf68b4ddd2b075e1b4f713bbd8d6b13d4
Gonzalo Monar <gonza.monar@gmail.com>
2025-07-23 20:52:02 -0300

    [user-service]chore: add RabbitMQ Config

---
+++ services/user-service/src/main/java/com/clinica_hipocrates/user_service/messaging/RabbitMQConfig.java
@@ -0,0 +1,43 @@
+package com.clinica_hipocrates.user_service.messaging;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableRabbit
+public class RabbitMQConfig {
+
+    public static final String USER_EXCHANGE = "user.exchange";
+    public static final String USER_REGISTERED_QUEUE = "user.registered.queue";
+    public static final String USER_REGISTERED_ROUTING_KEY = "user.registered";
+
+    @Bean
+    public DirectExchange userExchange() {
+        return new DirectExchange(USER_EXCHANGE);
+    }
+
+    @Bean
+    public Queue userRegisteredQueue() {
+        return new Queue(USER_REGISTERED_QUEUE, true);
+    }
+
+    @Bean
+    public Binding userRegisteredBinding() {
+        return BindingBuilder
+                .bind(userRegisteredQueue())
+                .to(userExchange())
+                .with(USER_REGISTERED_ROUTING_KEY);
+    }
+
+    @Bean
+    public MessageConverter jsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+}
