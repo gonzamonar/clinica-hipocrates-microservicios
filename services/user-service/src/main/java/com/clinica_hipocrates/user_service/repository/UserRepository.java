643cb48c773e22e6bd946a0268652b316e021afb
Gonzalo Monar <gonza.monar@gmail.com>
2025-07-23 20:50:08 -0300

    [user-service]refactor: adapt the SpecialistRepository functions to User

--- services/user-service/src/main/java/com/clinica_hipocrates/user_service/repository/UserRepository.java
+++ services/user-service/src/main/java/com/clinica_hipocrates/user_service/repository/UserRepository.java
@@ -4,8 +4,11 @@ import com.clinica_hipocrates.user_service.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByDni(Integer dni);
     boolean existsByEmail(String email);
+    List<User> findBySpecialities_Id(Long specialityId);
 }
