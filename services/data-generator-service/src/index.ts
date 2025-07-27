7fe8a68b3ae3abc5481db40b68b4f154257e2867
Gonzalo Monar <gonza.monar@gmail.com>
2025-07-27 12:55:17 -0300

    [data-generator-service]feat: add generator functions to index

--- services/data-generator-service/src/index.ts
+++ services/data-generator-service/src/index.ts
@@ -10,6 +10,34 @@ async function main() {
     const token = await login('gonza.monar@gmail.com', 'microservicios');
     const client = createClient(token);
 
+    /*
+    CREAR USUARIOS
+    for (let i = 0; i < 40; i++) {
+        await createPatient(client);
+        console.log(`Created patient ${i + 1}`);
+    }
+    
+    for (let i = 0; i < 10; i++) {
+        await createSpecialist(client);
+        console.log(`Created specialist ${i + 1}`);
+    }
+    
+    for (let i = 0; i < 5; i++) {
+        await createAdmin(client);
+        console.log(`Created admin ${i + 1}`);
+    }
+    */
+
+
+    // CREAR HORARIOS
+    
+
+    // CREAR TURNOS
+    //await createRandomAppointments(50, client);
+
+    // COMPLETAR TURNOS
+    //await finishAppointments(client);
+
 
     console.log("EXEC FINISHED");
 }
