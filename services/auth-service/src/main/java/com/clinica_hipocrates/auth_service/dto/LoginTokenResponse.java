d85cff4a42bb012b6e2b90143a56269c48b11b3b
Gonzalo Monar <gonza.monar@gmail.com>
2025-07-27 12:59:53 -0300

    [auth-service]feat: add LoginTokenResponse DTO

---
+++ services/auth-service/src/main/java/com/clinica_hipocrates/auth_service/dto/LoginTokenResponse.java
@@ -0,0 +1,12 @@
+package com.clinica_hipocrates.auth_service.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginTokenResponse {
+    private String token;
+}
